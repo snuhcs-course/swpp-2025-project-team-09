9786033c7e479a53161236267ddeca11a3378758
SohnEugene <qeugene2328@gmail.com>
2025-10-15 02:03:20 +0900

    fix: LandingActivity update

--- android-app/app/src/main/java/com/example/storybridge_android/LandingActivity.kt
+++ android-app/app/src/main/java/com/example/storybridge_android/LandingActivity.kt
@@ -31,13 +31,35 @@ class LandingActivity : AppCompatActivity() {
         }
 
         // Landing 화면 참조 (뒤에 어디에 사용되나요?)
+        // 처음 앱을 실행했을 때 landing 화면에서 voice preference까지 선택하도록 해야 해서
+        // landinglayout의 내용물을 toggle 하는 방식으로 구현해 놨습니다.
         landingLayout = findViewById(R.id.landing)
+
+        //TODO: 유저가 앱을 처음 열었는지 아닌지 확인해서 처음이라면 언어 설정.
 
         // 3초 후 Language Selection 화면으로 전환
         Handler(Looper.getMainLooper()).postDelayed({
             showLanguageSelection()
         }, 1000)
     }
+
+
+    private fun navigateToMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+
+    private fun checkIsRegistered() {
+        //TODO: 유저가 앱을 처음 열었는지 아닌지 확인
+    }
+
+    private fun setLangPreference() {
+        //TODO: 앱을 처음 열었더라면 언어 설정 + 적절한 화면 띄우기
+        //TODO: 이거 구현하고 나면 아래 showLanguageSelection은 지워 주세요
+    }
+
 
     private fun showLanguageSelection() {
         // Language Selection 레이아웃으로 전환
@@ -57,11 +79,4 @@ class LandingActivity : AppCompatActivity() {
         }
     }
 
-
-    // 버튼 클릭 시 MainActivity로 이동
-    private fun navigateToMain() {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
 }
