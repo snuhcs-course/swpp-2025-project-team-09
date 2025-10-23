4807605d4a0ea50a8c9177603f93fb775de6fe01
SohnEugene <qeugene2328@gmail.com>
2025-10-23 11:40:33 +0900

    add: test modules

---
+++ backend/tests/unit/models/test_user_model.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from apis.models.user_model import User
+
+
+class TestUserModel(TestCase):
+    """Unit tests for User model"""
+
+    def setUp(self):
+        """Set up test data before each test method"""
+        pass
+
+    def tearDown(self):
+        """Clean up after each test method"""
+        pass
+
+    def test_create_user(self):
+        """Test creating a new user"""
+        pass
+
+    def test_user_str_representation(self):
+        """Test string representation of user"""
+        pass
+
+    def test_user_fields(self):
+        """Test user model fields"""
+        pass
