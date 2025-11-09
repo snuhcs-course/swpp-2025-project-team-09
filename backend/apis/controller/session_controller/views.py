910e0dff522880ac6e78e2661f4c865f61f4ed7f
solmouse <sjkim.judy@gmail.com>
2025-11-09 18:14:53 +0900

    import prase_datatime

--- backend/apis/controller/session_controller/views.py
+++ backend/apis/controller/session_controller/views.py
@@ -4,6 +4,10 @@ from rest_framework import status
 from django.utils import timezone
 from apis.models.session_model import Session
 from apis.models.user_model import User
+from apis.models.page_model import Page
+from django.utils.dateparse import parse_datetime
+import base64
+
 
 
 class StartSessionView(APIView):
