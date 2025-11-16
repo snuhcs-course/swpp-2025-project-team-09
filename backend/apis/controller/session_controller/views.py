f2b20fab8d66dbcbd695c9167cc8bf8214e65ce2
Taeho Han <jackdoosan@snu.ac.kr>
2025-11-16 21:19:25 +0900

    linting

--- backend/apis/controller/session_controller/views.py
+++ backend/apis/controller/session_controller/views.py
@@ -213,8 +213,10 @@ class GetSessionInfoView(APIView):
                     "started_at": session.started_at,
                     "ended_at": session.ended_at,
                     "total_pages": session.totalPages,
-                    "total_time_spent": ((session.ended_at or timezone.now()) - session.started_at).seconds,
-                    "total_words_read": session.totalWords
+                    "total_time_spent": (
+                        (session.ended_at or timezone.now()) - session.started_at
+                    ).seconds,
+                    "total_words_read": session.totalWords,
                 },
                 status=status.HTTP_200_OK,
             )
@@ -273,8 +275,10 @@ class GetSessionStatsView(APIView):
                     "started_at": session.started_at,
                     "ended_at": session.ended_at,
                     "total_pages": session.totalPages,
-                    "total_time_spent": ((session.ended_at or timezone.now()) - session.started_at).seconds,
-                    "total_words_read": session.totalWords
+                    "total_time_spent": (
+                        (session.ended_at or timezone.now()) - session.started_at
+                    ).seconds,
+                    "total_words_read": session.totalWords,
                 },
                 status=status.HTTP_200_OK,
             )
@@ -328,8 +332,10 @@ class SessionReviewView(APIView):
                     "started_at": session.started_at,
                     "ended_at": session.ended_at,
                     "total_pages": session.totalPages,
-                    "total_time_spent": ((session.ended_at or timezone.now()) - session.started_at).seconds,
-                    "total_words_read": session.totalWords
+                    "total_time_spent": (
+                        (session.ended_at or timezone.now()) - session.started_at
+                    ).seconds,
+                    "total_words_read": session.totalWords,
                 },
                 status=status.HTTP_200_OK,
             )
