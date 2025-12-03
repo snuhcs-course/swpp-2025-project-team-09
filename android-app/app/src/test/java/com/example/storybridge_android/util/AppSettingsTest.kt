c6631a7c24ebc0a41c60a79a0c7d9891abb212bb
solmouse <sjkim.judy@gmail.com>
2025-12-03 09:56:48 +0900

    refactor: Simplify ViewModel tests and remove unused code
    # Conflicts:
    #	android-app/app/src/test/java/com/example/storybridge_android/view_model/CameraSessionViewModelTest.kt

--- android-app/app/src/test/java/com/example/storybridge_android/util/AppSettingsTest.kt
+++ android-app/app/src/test/java/com/example/storybridge_android/util/AppSettingsTest.kt
@@ -1,3 +1,5 @@
+package com.example.storybridge_android.util
+
 import android.content.Context
 import android.content.SharedPreferences
 import com.example.storybridge_android.ui.setting.AppSettings
