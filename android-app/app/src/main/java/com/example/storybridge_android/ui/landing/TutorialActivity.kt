868e28d02ab435dfd805868e87eefb815ecac44b
solmouse <sjkim.judy@gmail.com>
2025-12-07 14:12:11 +0900

    feat: Add word card step to tutorial

--- android-app/app/src/main/java/com/example/storybridge_android/ui/landing/TutorialActivity.kt
+++ android-app/app/src/main/java/com/example/storybridge_android/ui/landing/TutorialActivity.kt
@@ -76,7 +76,8 @@ class TutorialActivity : BaseActivity() {
             TutorialPage(R.drawable.tutorial_6, getString(R.string.tutorial_6)),
             TutorialPage(R.drawable.tutorial_7, getString(R.string.tutorial_7)),
             TutorialPage(R.drawable.tutorial_8, getString(R.string.tutorial_8)),
-            TutorialPage(R.drawable.tutorial_9, getString(R.string.tutorial_9))
+            TutorialPage(R.drawable.tutorial_9, getString(R.string.tutorial_9)),
+            TutorialPage(R.drawable.tutorial_10, getString(R.string.tutorial_10))
         )
     }
 
